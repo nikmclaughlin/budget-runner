e5cc65b3dcafc0d331a24757a2115fc624525e81
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2022-08-04 19:47:19 -0700

    StartingBalance component and basic styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,22 @@
 export default function Index() {
-  return (
+  
+	const StartingBalance = () => {
+		return (
+			<>
+				<form className="border border-dashed border-black w-1/2 p-1 my-5 relative flex-auto flex-wrap justify-center">
+					<h2 className="text-2xl grow">Set your starting balance</h2>
+					<label htmlFor="balance" className="-mr-2 text-xl">$</label>
+					<input id="balance" name="balance" className="border border-solid border-black mx-2" type="number" step="0.01" defaultValue={"0.00"}></input>
+					<button className="rounded px-2 py-1 border border-solid border-black bg-sky-700">Set</button>
+				</form>
+			</>
+		)
+	}
+
+	return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1 className="text-5xl">Budget Runner 👟</h1>
+			<StartingBalance />
       <ul>
         <li>
           <a
