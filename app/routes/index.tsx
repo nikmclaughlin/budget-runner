a38d6086394099bbda39f337ca1bc6c4c6a3ab01
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2022-08-08 17:48:47 -0700

    Basic HTML outline for output table

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -60,11 +60,43 @@ export default function Index() {
 
 	}
 
+	const OutputTable = () => {
+		const today = new Date(Date.now());
+		return(
+			<table className="table-fixed border-collapse border border-solid border-black">
+				<thead className="bg-gray-500">
+					<tr>
+						<th className="w-40">Date</th>
+						<th>Item</th>
+						<th>Value</th>
+						<th>Balance</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>{today.toLocaleDateString()}</td>
+						<td>Starting Balance</td>
+						<td>+100.00</td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		);
+	}
+
 	return (
-    <div>
-      <h1 className="text-5xl">Budget Runner 👟</h1>
-			<StartingBalance />
-			<ItemEditor />
+		<>
+		<h1 className="text-5xl">Budget Runner 👟</h1>
+    <div className="grid grid-cols-2">
+			<div className="col-span-1">
+				<StartingBalance />
+				<ItemEditor />
+			</div>
+			<div className="col-span-1">
+				<h1 className="text-4xl">Budget output</h1>
+				<OutputTable />
+			</div>
     </div>
+		</>
   );
 }
