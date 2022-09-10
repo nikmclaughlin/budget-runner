7e65d4dfa44c43a6f6482c52dd70174124991416
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2022-09-09 20:46:47 -0700

    WIP: processing addItem form data

--- app/routes/user/__inputs/addItem.tsx
+++ app/routes/user/__inputs/addItem.tsx
@@ -7,13 +7,15 @@ export async function action({ request }){
 	// validate data
 
 	//prepare data for db
-	const submittedFields = Object.fromEntries(body);
-	console.log(submittedFields);
+	// const submittedFields = Object.fromEntries(body);
+	// submittedFields.
+	// console.log(submittedFields);
 
-	// submit data to db
-	const newItem = await db.lineItem.create({
-		data: submittedFields ,
-});
+
+	// // submit data to db
+	// const newItem = await db.lineItem.create({
+// 		data: submittedFields ,
+// });
 
 	return null;
 
@@ -33,9 +35,9 @@ const ItemAdd = () => {
 					<legend className="text-2xl grow ">Add a line item</legend>
 					<input type="text" name="label" placeholder="Item Label"></input>
 					<input type="number" id="amount" name="amount" step="0.01" placeholder="$00.00" className="w-20"></input>
-					<input type="radio" id="plus" name="sign" value="positive"></input>
+					<input type="radio" id="plus" name="sign" value="true"></input>
 					<label htmlFor="plus">+</label>
-					<input type="radio" id="minus" name="sign" value="negative"></input>
+					<input type="radio" id="minus" name="sign" value="false"></input>
 					<label htmlFor="minus">-</label>
 					{/*TODO: Turn the 'sign' selector into a toggle, e.g.: https://flowbite.com/docs/forms/toggle/ */}
 					<br></br>
@@ -43,8 +45,8 @@ const ItemAdd = () => {
 					<input type="date" id="startDate" name="startDate" className="border border-black rounded"></input>
 					<br></br>
 					<p className="font-bold">Frequency</p>
-					<span>This occurs every </span><input type="number" name="freqCount" step="1" className="w-10 border border-black rounded"></input>
-					<select id="freqUnit" name="freqUnit">
+					<span>This occurs every </span><input type="number" name="frequencyVal" step="1" className="w-10 border border-black rounded"></input>
+					<select id="freqUnit" name="frequencyUnit">
 						<option value="days">days</option>
 						<option value="weeks">weeks</option>
 						<option value="months">months</option>
@@ -52,7 +54,7 @@ const ItemAdd = () => {
 					</select>
 					<br></br>
 					<input type="number" id="duration" name="duration" step="1" title="Leave blank for a never-ending item" className="w-10 border border-black rounded"></input>
-					<label htmlFor="duration" title="Leave blank for a never-ending item" className="font-bold">Duration (optional)</label>
+					<label htmlFor="duration" title="Leave blank for a never-ending item" defaultValue={''} className="font-bold">Duration (optional)</label>
 					<br></br>
 					<input type="submit" value="Add to budget" className="rounded px-2 py-1 border border-solid border-black bg-green-800"></input>
 				</fieldset>
